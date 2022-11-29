3b4f0961c836aad21b63c0f84f6f9b6a0f9c1b7b
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-11-29 03:05:22 -0800

    2-print_alphabet.py

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+rem = number % 10
+base = "Last digit of "+str(number) + " is " + str(rem)
+if rem > 5:
+    print(f"{base} and is greater than 5")
+elif rem == 0:
+    print(f"{base} and is 0")
+elif rem < 6:
+    print(f"{base} and is less than 6 and not 0")
