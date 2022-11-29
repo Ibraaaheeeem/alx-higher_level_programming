1e7b26b9f2e8f55fd7d553525b0d553a05ba9b04
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-11-29 09:56:54 -0800

    Correct pycodestyle

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,6 +4,6 @@ def print_last_digit(number):
     if number < 0:
         lastN = ((number * -1)) % 10
     else:
-        lastN =  number % 10
+        lastN = number % 10
     print("{:d}".format(lastN), end="")
     return lastN
