cef6e6be7a059d9b4810196b21959f2b858b5695
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-11-29 11:57:57 -0800

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+flag = True;
+stringZYX = ""
+for i in range(ord("z"), ord("a")-1, -1):
+    if flag:
+        # print("{:c}".format(i), end="")
+        stringZYX += chr(i)
+    else:
+        # print("{:c}".format((i-32), end=""))
+        stringZYX += chr(i - 32)
+    flag = not flag
+print(stringZYX, end="")
