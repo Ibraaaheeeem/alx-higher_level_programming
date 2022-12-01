3e451b869301a76a14337608811fcd777c8e3476
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-12-01 09:48:11 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    l = len(sys.argv) - 1
+    if l == 0:
+        print("{:d} arguments.".format(l))
+    elif l == 1:
+        print("{:d} argument:".format(l))
+    else:
+        print("{:d} arguments".format(l))
+    for i in range(1, len(sys.srgv)-1):
+        print("{:d}: {:s}".format(i, sys.argv[i])
