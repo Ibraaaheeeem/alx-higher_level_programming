546d600c6c1bae73b5947362d2d038ad6012cb38
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-12-05 10:57:59 -0800

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    noC = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'C' and my_string[i] != 'c':
+            noC += my_string[i]
+    return noC
