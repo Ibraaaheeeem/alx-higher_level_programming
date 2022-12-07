a0c12bce544aa4d8ab0720e09e86045a30003073
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-12-07 11:23:02 -0800

    Correct error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = sorted(a_dictionary.items(), key = lambda x: x[0])
-    return dict(sorted_dict)
+    for k in sorted_dict:
+        print("{}: {}".format(k, sorted_dict[k]))
