080b5f92fdbeebe6a5a65c4f0c1783f993e20559
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-12-07 11:36:44 -0800

    10-mai.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None or a_dictionary == {}:
+        return None
+    max_value = max(a_dictionary.values())
+    for key in a_dictionary:
+        if a_dictionary[key] == max_value:
+            return key
